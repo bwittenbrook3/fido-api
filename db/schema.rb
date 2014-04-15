df4b6c4fb9604248ee3150cb32ac8f1750bff969
Bradley Wittenbrook <bradley.wittenbrook@gmail.com>
2014-04-15 23:22:54 +0000

    Added training model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140415030531) do
+ActiveRecord::Schema.define(version: 20140415232217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -33,6 +33,13 @@ ActiveRecord::Schema.define(version: 20140415030531) do
     t.text     "details"
     t.float    "latitude"
     t.float    "longitude"
+  end
+
+  create_table "trainings", force: true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
   create_table "users", force: true do |t|
