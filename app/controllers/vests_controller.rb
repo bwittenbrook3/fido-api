22ae40aa7ee89ff8feb8f7968320fd90bac694a3
Bradley Wittenbrook <bradley.wittenbrook@gmail.com>
2014-04-15 20:09:19 +0000

    Ajaxified K9 page.

--- app/controllers/vests_controller.rb
+++ app/controllers/vests_controller.rb
@@ -2,4 +2,12 @@ class VestsController < ApplicationController
 	def index
 		@vests = Vest.all
 	end
+
+	def show
+		@vest = Vest.find(params[:id])
+
+		respond_to do |format|
+			format.js
+		end
+	end
 end
