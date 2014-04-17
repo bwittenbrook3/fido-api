e6b78b879772469f704126e5ac2f69f375294d8a
Bradley Wittenbrook <bradley.wittenbrook@gmail.com>
2014-04-17 05:28:32 +0000

    Cool map effects.

--- app/controllers/map_controller.rb
+++ app/controllers/map_controller.rb
@@ -1,5 +1,9 @@
 class MapController < ApplicationController
 	def index
-		@events = Event.near(request.location, 100)
+		@events = Event.near(request.location.coordinates, 25)
+		@hash = Gmaps4rails.build_markers(@events) do |event, marker|
+		  marker.lat event.latitude
+		  marker.lng event.longitude
+		end
 	end
 end
